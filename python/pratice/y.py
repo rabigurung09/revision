d8ff61aaf5119fb37d4b144351e1142fdd2e1296
rabigurung09 <rabi72732@gmail.com>
2026-08-18 15:33:08 +0545

    pratice

---
+++ python/pratice/y.py
@@ -0,0 +1,9 @@
+def name():
+    print("rabi")
+name()
+"""jdshfhjsgfjhgdfhjgsfjhgshjhsdhjsdgfhj"""
+user = int(input("enter your number: "))
+user_1= int(input("enter your num :"))
+add = user+ user_1
+print(add)
+
